23a80fc4ed9157dd54315d284b66b0da6c5b07a2
Maksym Rubtsov <maksrub04@gmail.com>
2025-06-09 13:27:38 +0200

    racer

---
+++ APBD_TEST_2_C/DAL/DTOs/RacerParticipationsDTO.cs
@@ -0,0 +1,9 @@
+namespace APBD_TEST_2_C.DAL.DTOs;
+
+public class RacerParticipationsDTO
+{
+    public int RacerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public List<ParticipationDetailDTO> Participations { get; set; }
+}
